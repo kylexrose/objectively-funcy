9541f126fcb8e08e443790aaa0fc8d7f7976d328
kylexrose@gmail.com <kylexrose@gmail.com>
2021-03-11 14:34:46 -0500

    completed all functions and passed all tests

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // Your code here.
+var getFirstName = (obj) => obj.firstName;
 
+var getLastName = (obj) => obj.lastName;
 
+var getFullName = (obj) => obj.firstName + " " + obj.lastName;
 
+var setFirstName = (obj, str) => obj.firstName = str;
 
+var setAge = (obj, num) => obj.age = num;
 
+var giveBirthday = (obj) => {
+  if(obj.age){
+    obj.age++;
+  }else{
+    obj.age = 1;
+  }
+}
+
+var marry = (obj1, obj2) => {
+  obj1.married = true;
+  obj1.spouseName = getFullName(obj2);
+  obj2.married = true;
+  obj2.spouseName = getFullName(obj1);
+}
+
+var divorce = (obj1, obj2) => {
+  obj1.married = false;
+  obj2.married = false;
+  delete obj1.spouseName;
+  delete obj2.spouseName;
+}
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
   getFirstName = undefined
